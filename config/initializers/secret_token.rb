a8e18c6b39008c96c097b862d961c9cf6220bf1e
tingwu <tingwu@usc.edu>
2010-11-27 19:56:42 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TingApp::Application.config.secret_token = 'fceb43ec1dff82a38c0744585b8ac08f39bb46f44b59ed37988a4bc6688738e9582e625db630d0c9f9659100dbe309560f4ee8c51eafa0c4a10ff759324183b9'
